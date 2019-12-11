7e7e32cfe938b388ece018f029c59f65b683f47c
Ryan Pilgrim <ryan.pilgrim@infiniaml.com>
2019-12-10 21:51:36 -0500

    checkpoint before removing grid

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,31 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Day3 where
 
-import qualified Data.Text as T
-import qualified Data.Map as M
-import qualified Data.Array as A
-import System.IO (FilePath)
+import qualified Data.Text                     as T
+import qualified Data.Map                      as M
+import qualified Data.Array                    as A
+import           System.IO                      ( FilePath )
 
 data Direction = U | D | L | R | InvalidDirection deriving (Show, Eq)
 data Distance = Distance Int | InvalidDistance deriving (Show, Eq, Ord)
 
 makeDistance :: T.Text -> Distance
-makeDistance s
-  | dist < 0 = InvalidDistance
-  | otherwise = Distance dist
-  where dist :: Int
-        dist = read $ T.unpack s
+makeDistance s | dist < 0  = InvalidDistance
+               | otherwise = Distance dist
+ where
+  dist :: Int
+  dist = read $ T.unpack s
 
 toInt :: Distance -> Int
 toInt InvalidDistance = 0
-toInt (Distance x) = x
+toInt (Distance x)    = x
 
 toDirection :: T.Text -> Direction
 toDirection "U" = U
 toDirection "D" = D
 toDirection "L" = L
 toDirection "R" = R
-toDirection _ = InvalidDirection
+toDirection _   = InvalidDirection
 
 data WirePathComponent = InvalidWirePathComponent | WirePathComponent
   { wpDirection :: Direction
@@ -36,19 +36,20 @@ type WirePath = [WirePathComponent]
 type WirePaths = [WirePath]
 
 parseWirePathComponent :: T.Text -> WirePathComponent
-parseWirePathComponent s 
+parseWirePathComponent s
   | dir == InvalidDirection = InvalidWirePathComponent
   | dist == InvalidDistance = InvalidWirePathComponent
-  | otherwise = WirePathComponent {wpDirection=dir, wpDistance=dist} 
-  where
-    dir = toDirection $ T.take 1 s
-    dist = makeDistance $ T.drop 1 s
+  | otherwise = WirePathComponent { wpDirection = dir, wpDistance = dist }
+ where
+  dir  = toDirection $ T.take 1 s
+  dist = makeDistance $ T.drop 1 s
 
 parseWirePath :: T.Text -> WirePath
 parseWirePath directions = parseWirePathComponent <$> T.splitOn "," directions
 
 parseWirePaths :: T.Text -> WirePaths
-parseWirePaths directions = parseWirePath <$> takeWhile (not . T.null) (T.splitOn "\n" directions)
+parseWirePaths directions =
+  parseWirePath <$> takeWhile (not . T.null) (T.splitOn "\n" directions)
 
 readWirePathsFromFile :: FilePath -> IO WirePaths
 readWirePathsFromFile filePath = parseWirePaths . T.pack <$> readFile filePath
@@ -59,7 +60,8 @@ data Axis = XAxis | YAxis deriving (Show, Eq)
 
 signedMagnitude :: WirePathComponent -> Int
 signedMagnitude wpComponent
-  | wpDirection wpComponent == InvalidDirection = error "InvalidDirection passed to sign."
+  | wpDirection wpComponent == InvalidDirection = error
+    "InvalidDirection passed to sign."
   | wpDirection wpComponent == U = 1 * magnitude
   | wpDirection wpComponent == D = -1 * magnitude
   | wpDirection wpComponent == L = -1 * magnitude
@@ -68,20 +70,23 @@ signedMagnitude wpComponent
 
 axis :: Direction -> Axis
 axis InvalidDirection = error "InvalidDirection passed to axis."
-axis U = YAxis
-axis D = YAxis
-axis L = XAxis
-axis R = XAxis
+axis U                = YAxis
+axis D                = YAxis
+axis L                = XAxis
+axis R                = XAxis
 
 extremumForAxisAndPath :: Axis -> Extremum -> WirePath -> Int
-extremumForAxisAndPath ax extremum path = sum $ filter filterFunc distancesInDirection where
-  distancesInDirection = map signedMagnitude $ filter ((== ax) . axis . wpDirection) path
-  filterFunc = case extremum of 
-                 High -> (> 0)
-                 Low -> (< 0)
+extremumForAxisAndPath ax extremum path = sum
+  $ filter filterFunc distancesInDirection where
+  distancesInDirection =
+    map signedMagnitude $ filter ((== ax) . axis . wpDirection) path
+  filterFunc = case extremum of
+    High -> (> 0)
+    Low  -> (< 0)
 
 extremumForAxis :: Axis -> Extremum -> WirePaths -> Int
-extremumForAxis ax extremum = maximum . map (extremumForAxisAndPath ax extremum)
+extremumForAxis ax extremum =
+  maximum . map (extremumForAxisAndPath ax extremum)
 
 data GridSize = GridSize
   { gXMin :: Int
@@ -96,6 +101,13 @@ gridSize wps = GridSize xmin ymin xmax ymax where
   ymin = extremumForAxis YAxis Low wps
   xmax = extremumForAxis XAxis High wps
   ymax = extremumForAxis YAxis High wps
+
+
+
+
+
+
+
 
 
 
